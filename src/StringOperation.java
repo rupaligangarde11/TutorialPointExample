6640a8d9daa50f812a15853a27ab0670c61418d0
Rupali <rupalig@thoughtworks.com>
2016-04-29 11:43:01 +0530

    unicode example

--- src/StringOperation.java
+++ src/StringOperation.java
@@ -1,7 +1,10 @@
 public class StringOperation {
     public  static void main(String args[]){
+
+
         String firstString = "Hello World, Hello Reader";
         String secondString = "I said Hello";
+
         int position =3;
 
         searchLastString(firstString);
@@ -9,10 +12,27 @@ public class StringOperation {
         reverseString(firstString);
         splitString(firstString);
 
-
         matchesRegion(firstString, secondString);
+        measureStringLiteralCreationTime();
+
+        unicodeAt(firstString);
+
 
         return;
+    }
+
+    private static void unicodeAt(String firstString) {
+        System.out.println("unicode code point at position 5 in string is : " + firstString.codePointAt(5));
+    }
+
+    private static void measureStringLiteralCreationTime() {
+        long startTime = System.currentTimeMillis();
+        for (int i =0;i<50000;i++){
+            String str1= "Hie";
+            String str2 = "Hello";
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("time for string literals creation : " +(endTime-startTime));
     }
 
     private static void matchesRegion(String firstString, String secondString) {
