05637b45c668244d7a72b86213725246a7a7d893
Rupali <rupalig@thoughtworks.com>
2016-04-29 12:38:02 +0530

    isFileCreated example

--- src/FileOperation.java
+++ src/FileOperation.java
@@ -1,11 +1,27 @@
 import java.io.File;
+import java.io.IOException;
 
 public class FileOperation {
 
     public static void main(String[] args) {
         File firstFile = new File("/Users/rupalig/TWU/TutorialPointExamples/src/file1");
         File secondFile = new File("/Users/rupalig/TWU/TutorialPointExamples/src/file2");
+        File thirdFile = new File("/Users/rupalig/TWU/TutorialPointExamples/src/file3");
+
+        try {
+            isFileCreated(thirdFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         compareFileBasedOnFilePath(firstFile, secondFile);
+    }
+
+    private static void isFileCreated(File file) throws IOException {
+        if(file.createNewFile())
+            System.out.println("file created");
+        else
+            System.out.println("file didn't create");
     }
 
     private static void compareFileBasedOnFilePath(File firstFile, File secondFile) {
